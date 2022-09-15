319d182c9fd210a8d172d3e1a20d7624c76285b2
PenPeen <hiroto@makkunoMacBook-Air.local>
2022-09-15 10:58:41 +0900

    利便性の向上

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # get: 
-
+  root "questions#index"
   resources :questions
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
